246848d352eaa9575c15de3038e472f39fb208f0
iamyare <yarephotographer@gmail.com>
2024-02-12 16:13:58 -0600

    Implementar la funcionalidad de inicio de sesión Welcome

---
+++ app/(pages)/welcome/welcome.tsx
@@ -0,0 +1,85 @@
+
+'use client'
+import Confetti from '@/components/confetti'
+
+import { Dialog, Transition } from '@headlessui/react'
+import Link  from 'next/link'
+import { Fragment } from 'react'
+
+export default function WelcomeClient({user} : {user: Personas}) {
+
+  return (
+    <>
+      <Transition appear show={true} as={Fragment}>
+        <Dialog as="div" className="relative z-10" static onClose={() => {}}>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-500"
+                enterFrom="opacity-0 scale-90"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white/50 dark:bg-black/50 p-6 text-left align-middle shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg text-center font-medium leading-6 text-gray-900 dark:text-gray-100"
+                  >
+                    Bienvenidos a Saim Cis 🎉
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-center text-gray-800 dark:text-gray-200">
+                    ¡Gracias por unirte a nosotros <strong>{user.primer_nombre}</strong>!
+                    </p>
+                  </div>
+
+                  <div className="mt-4 flex justify-center">
+                    <Link
+                    href={`/${user.rol}`}
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 dark:bg-blue-900 px-4 py-2 text-sm font-medium text-blue-900 dark:text-blue-100 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                    >
+                        Empezar
+                    </Link>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+
+        <Confetti/>
+
+        <div
+            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)'
+              }}
+            />
+
+          </div>
+
+          <div
+            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)'
+              }}
+            />
+          </div>
+
+    </>
+  )
+}
