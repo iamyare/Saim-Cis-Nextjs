f3638dfd398d7d1348b35c1e154ec4a6f325b2ba
iamyare <yarephotographer@gmail.com>
2024-02-21 22:05:29 -0600

    Actualizar función getUser para obtener información adicional del usuario

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -2,13 +2,44 @@ import { readUserSession } from "@/lib/actions";
 import { supabase } from "@/lib/supabase";
 
 export async function getUser({ id }: { id: string }) {
+  let usuarioModificado;
   const { data: usuario, error: errorUsuario } = await supabase
-    .from("personas")
-    .select("*")
-    .eq("id", id)
+    .from("personas_x_usuarios")
+    .select(
+      "*, personas(*, especializaciones:especializacion_x_personas(especializacion:especializaciones(nombre, rol:roles(nombre))))"
+    )
+    .eq("id_usuario", id)
     .single();
 
-  return { usuario, errorUsuario };
+  if (usuario && usuario.personas) {
+    const { personas, ...restoUsuario } = usuario;
+    const { especializaciones, ...restoPersonas } = personas;
+
+    const roles = especializaciones.reduce((acc, especializacion) => {
+      const rolNombre = especializacion.especializacion?.rol?.nombre || "";
+      const especialidad = especializacion.especializacion?.nombre || "";
+
+      const rolExistente = acc.find((rol) => rol.rol === rolNombre);
+
+      if (rolExistente) {
+        rolExistente.especialidad.push(especialidad);
+      } else {
+        acc.push({ rol: rolNombre, especialidad: [especialidad] });
+      }
+
+      return acc;
+    }, [] as { rol: string; especialidad: string[] }[]);
+
+    usuarioModificado = {
+      ...restoPersonas,
+      role: roles.length > 0 ? roles : [{ rol: "", especialidad: [""] }],
+      usuario: {
+        ...restoUsuario,
+      },
+    };
+  }
+
+  return { usuario: usuarioModificado, errorUsuario };
 }
 
 export async function getExpedienteByUser({ id }: { id: string }) {
