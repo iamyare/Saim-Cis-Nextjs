07fe3bb4e60c214227fb3717b55ba6e5108a5c3f
iamyare <yarephotographer@gmail.com>
2024-02-21 23:07:14 -0600

    V=2.8.1.U
    Modificaciones de colores en el Sidebar

--- app/(pages)/doctor/layout.tsx
+++ app/(pages)/doctor/layout.tsx
@@ -1,11 +1,11 @@
-'use client'
-import {SidebarNavigation} from '@/components/sidebar-navigation'
-import React from 'react'
+"use client";
+import { SidebarNavigation } from "@/components/sidebar-navigation";
+import React from "react";
 
-export default function Layout({children}: {children : React.ReactNode}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <SidebarNavigation />
     </>
-  ) 
+  );
 }
