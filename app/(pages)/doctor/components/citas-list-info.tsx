8bcad6636646b6bde32a747e2fc495966e7ea0d4
cruz-IS <109125010+cruz-IS@users.noreply.github.com>
2024-02-27 10:51:45 -0600

    V=2.5.3.U
    Se agrego el boton de cerrar el modal de las citas.

--- app/(pages)/doctor/components/citas-list-info.tsx
+++ app/(pages)/doctor/components/citas-list-info.tsx
@@ -49,58 +49,86 @@ export default function CitasListInfo({
         <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto">
           <div className="bg-white border border-gray-200 rounded-xl pointer-events-auto shadow-sm dark:bg-gray-800 dark:border-gray-700">
             <div className="p-4 sm:p-7">
+              <div className="text-center flex justify-end">
+                <button
+                  type="button"
+                  className="flex justify-center items-center size-7 text-sm font-semibold rounded-lg border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-transparent dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                  data-hs-overlay="#hs-modal-cita"
+                >
+                  <span className="sr-only">Close</span>
+                  <svg
+                    className="flex-shrink-0 size-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M18 6 6 18" />
+                    <path d="m6 6 12 12" />
+                  </svg>
+                </button>
+              </div>
               <div className="text-center">
                 <h2 className="block text-2xl font-bold text-gray-800 dark:text-gray-200">
                   Cita
                 </h2>
-                <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                <p className="mt-2 text-sm text-gray-700 dark:text-gray-300">
                   Informacion detallada de la cita
                 </p>
               </div>
               <div className="mt-5">
                 <div className="flex flex-col gap-4">
-                  <div className="flex flex-col gap-1">
-                    <span className="text-sm font-semibold">Paciente</span>
-                    <span className="text-sm">
-                      {citaSelected?.paciente?.nombre}{" "}
-                      {citaSelected?.paciente?.apellido}
-                    </span>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4 text-justify">
+                    <div className="flex flex-col gap-1">
+                      <span className="text-sm font-semibold">Paciente</span>
+                      <span className="text-sm">
+                        {citaSelected?.paciente?.nombre}{" "}
+                        {citaSelected?.paciente?.apellido}
+                      </span>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                      <span className="text-sm font-semibold">Fecha</span>
+                      <span className="text-sm">
+                        {new Date(
+                          citaSelected?.fecha_inicio ?? ""
+                        ).toLocaleDateString("es-HN", {
+                          weekday: "long",
+                          day: "numeric",
+                          month: "long",
+                        })}
+                      </span>
+                    </div>
                   </div>
-                  <div className="flex flex-col gap-1">
-                    <span className="text-sm font-semibold">Fecha</span>
-                    <span className="text-sm">
-                      {new Date(
-                        citaSelected?.fecha_inicio ?? ""
-                      ).toLocaleDateString("es-HN", {
-                        weekday: "long",
-                        day: "numeric",
-                        month: "long",
-                      })}
-                    </span>
-                  </div>
-                  <div className="flex flex-col gap-1">
-                    <span className="text-sm font-semibold">Hora</span>
-                    <span className="text-sm">
-                      {new Date(
-                        citaSelected?.fecha_inicio ?? ""
-                      ).toLocaleTimeString("es-HN", {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                      })}
-                      {" - "}
-                      {new Date(
-                        citaSelected?.fecha_final ?? ""
-                      ).toLocaleTimeString("es-HN", {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                      })}
-                    </span>
-                  </div>
-                  <div className="flex flex-col gap-1">
-                    <span className="text-sm font-semibold">Descripcion</span>
-                    <span className="text-sm">
-                      {citaSelected?.descripcion ?? "Sin descripcion"}
-                    </span>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4 text-justify">
+                    <div className="flex flex-col gap-1">
+                      <span className="text-sm font-semibold">Hora</span>
+                      <span className="text-sm">
+                        {new Date(
+                          citaSelected?.fecha_inicio ?? ""
+                        ).toLocaleTimeString("es-HN", {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                        {" - "}
+                        {new Date(
+                          citaSelected?.fecha_final ?? ""
+                        ).toLocaleTimeString("es-HN", {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </span>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                      <span className="text-sm font-semibold">Descripcion</span>
+                      <span className="text-sm">
+                        {citaSelected?.descripcion ?? "Sin descripcion"}
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
