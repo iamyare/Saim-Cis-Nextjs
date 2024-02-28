4ea6a3b51409d1597a5a11d0402bc8d4a6848df8
haru-chan25 <haroldvasquez49@gmail.com>
2024-02-28 13:42:37 -0600

    V = 3.2.1.U
    Actualizacion de codigo

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import NavbarIndex from "@/components/navbar-index";
-import ServicesSection from "./components/services";
-import DoctorsSection from "./components/doctors";
-import Carousel from "./components/carousel";
-import Footer from "./components/footer";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
-import Nosotros from "./components/nosotros";
-import { ModalCitas } from "./(pages)/doctor/components/modal-citas";
+import NavbarIndex from '@/components/navbar-index'
+import ServicesSection from './components/services'
+import DoctorsSection from './components/doctors'
+import Carousel from './components/carousel'
+import Footer from './components/footer'
+// import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import Nosotros from './components/nosotros'
+// import { ModalCitas } from './(pages)/doctor/components/modal-citas'
 
 export default function Home () {
   return (
@@ -20,8 +20,8 @@ export default function Home () {
       <ServicesSection />
       <DoctorsSection />
       <Footer />
-      {/* <div className="grid place-items-center h-screen">
-      <h2 className="text-2xl">Landing Page 🚀</h2>
+      {/* <div className='grid place-items-center h-screen'>
+      <h2 className='text-2xl'>Landing Page 🚀</h2>
       </div> */}
     </>
   )
