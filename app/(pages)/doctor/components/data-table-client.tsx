ed2e12a28ea8aad9b3582839761f48e509cbfa34
esau-bg <103141322+esau-bg@users.noreply.github.com>
2024-03-03 11:34:41 -0600

    V = 3.4.1.U
    - se modificó la paginación y filtrado para que traiga todas las consultas (vista de médico) a excepción de las que tienen el estado "completada"

--- app/(pages)/doctor/components/data-table-client.tsx
+++ app/(pages)/doctor/components/data-table-client.tsx
@@ -94,7 +94,7 @@ export default function DataTableClient ({ consultas }: { consultas: InfoConsult
                             Ver perfil
                         </Link>
                         <button data-hs-overlay="#hs-modal-preclinica" className="flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:bg-gray-700" >
-                            Agregar Consulta
+                            Atender
                         </button>
                       </div>
                     </div>
